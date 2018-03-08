719000ff3a9736c9594faf2f6a3bc65ca38959dc
Kevin Kelly <kvinklly@gmail.com>
2018-03-08 10:01:22 -0600

    addes GET /api/boards route

--- server/api/boards.spec.js
+++ server/api/boards.spec.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 const db = require('../db')
-const BulletinBoard = db.model('Board')
+const Board = db.model('Board')
 const app = require('../index')
 const request = require('supertest')
 
@@ -9,9 +9,19 @@ describe('Board routes', () => {
     return db.sync({ force: true })
   })
 
-  describe('/api/boards', () => {
-    const boards = [
+  describe('GET /api/boards', () => {
+    const boards = [{ title: 'Pictures'}, { title: 'Interesting Links' }]
 
-    ]
+    beforeEach(() => Board.bulkCreate(boards))
+
+    it('should return all the boards', () => {
+      return request(app)
+        .get('/api/boards')
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('array')
+          expect(res.body.length).to.be.equal(2)
+        })
+    })
   })
 })
