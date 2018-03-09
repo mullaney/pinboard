8082468b12c6f5b0b9fbb38cad08ed47e37a31a2
Kevin Kelly <kvinklly@gmail.com>
2018-03-08 21:52:17 -0600

    add prop types to Pin and removed navbar

--- client/components/Board.js
+++ client/components/Board.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { fetchBoard } from '../store'
 import { Pin } from './'
-// import { NavLink } from 'react-router-dom'
 
 export class Board extends Component {
   componentDidMount() {
--- client/routes.js
+++ client/routes.js
@@ -20,17 +20,14 @@ class Routes extends Component {
       <Switch>
         <Route exact path="/boards" component={Boards} />
         <Route exact path="/boards/:id" component={Board} />
-        {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {
           isLoggedIn &&
             <Switch>
-              {/* Routes placed here are only available after logging in */}
               <Route path="/home" component={UserHome} />
             </Switch>
         }
-        {/* Displays our Login component as a fallback */}
         <Route component={Boards} />
       </Switch>
     )
